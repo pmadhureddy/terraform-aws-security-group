8815eea7f9528b1312cd5d8b9a202b65d09b1624
pmadhureddy <pmadhureddy1510@gmail.com>
2024-10-06 11:21:17 +0530

    Giving output ID to the module user

--- main.tf
+++ main.tf
@@ -1,5 +1,5 @@
-resource "aws_security_group" "allow_tls" {
-  name        = "allow_tls"
+resource "aws_security_group" "main" {
+  name        = local.sg_final_name
   description = "Allow TLS inbound traffic and all outbound traffic"
   vpc_id      = var.vpc_id
 
